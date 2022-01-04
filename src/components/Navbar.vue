f54080b4222069c565e1d62a942baee56130ad93
DollyChen_CYC <dolly726033@gmail.com>
2022-01-04 14:37:21 +0800

    feat: create Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav>
+    <h1 class="nav-title"><router-link to="home">Dolly Chen</router-link></h1>
+    <ul class="nav-list">
+      <li class="nav-list__item">
+        <router-link to="home">Portfolio</router-link>
+      </li>
+      <li class="nav-list__item">
+        <a
+          href="https://www.linkedin.com/in/dolly-yiching-chen/"
+          target="_blank"
+          >Linkedin</a
+        >
+      </li>
+      <li class="nav-list__item">
+        <a href="https://github.com/DollyChen-CYC" target="_blank">Github</a>
+      </li>
+      <li class="nav-list__item">
+        <a href="https://dollychen-yiching.medium.com/" target="_blank">Blog</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  background: #0e76a8;
+  padding: 20px 30px;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    cursor: pointer;
+    &:hover,
+    &:active {
+      color: inherit;
+      text-decoration: underline;
+      outline: none;
+    }
+  }
+
+  .nav-list {
+    list-style-type: none;
+    display: flex;
+    gap: 20px;
+    font-weight: 500;
+    font-size: 18px;
+  }
+}
+</style>
